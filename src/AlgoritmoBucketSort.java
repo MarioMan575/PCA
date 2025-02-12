95a3ce42b6bca92597d547ce85a238974d0791ad
Mario Manzanares Gala <151616727+MarioMan575@users.noreply.github.com>
2025-02-12 14:35:22 +0100

    Add files via upload

---
+++ src/AlgoritmoBucketSort.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import practicas_pca.TesterRun;
+
+public class AlgoritmoBucketSort implements TesterRun {
+    @Override
+    public ArrayList<Integer> bucketSort(List<Integer> numbers, int num_threads) {
+        /* CODIGO A COMPLETAR POR EL ALUMNO */
+        ArrayList<Integer> sorted = new ArrayList<Integer>();
+        System.out.println("Hola mundo");
+        return sorted;
+    }
+}
